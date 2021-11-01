1730b3c430e88165172d2fde01d8e7802c865746
redstrate <git@mail.redstrate.com>
2021-11-01 14:52:34 -0400

    Properly set org/app names for config files * Adds separate debug/release config files (doesn't happen for passwords)

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,16 @@
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationName("redstrate");
+    QCoreApplication::setOrganizationDomain("com.redstrate");
+
+#ifdef NDEBUG
+    QCoreApplication::setApplicationName("xivlauncher");
+#else
+    QCoreApplication::setApplicationName("xivlauncher-debug");
+#endif
+
     LauncherWindow w;
     w.show();
 
