c925fc53425ffd64e9e4c1c81e86f4309436e577
Joshua Goins <josh@redstrate.com>
2022-04-14 19:02:14 -0400

    Fix encrypted arguments on macOS failing
    It seems something changed in recent Wine versions(?) so there's now
    a new TickCount() function using mach_continuous_time() based off of
    XIV on Mac's implementation.

--- src/encryptedarg.cpp
+++ src/encryptedarg.cpp
@@ -10,12 +10,16 @@
 #endif
 
 #if defined(Q_OS_MAC)
-// this is pretty much what wine does :-0
+// taken from XIV-on-Mac, apparently Wine changed this?
 uint32_t TickCount() {
-    struct mach_timebase_info convfact;
-    mach_timebase_info(&convfact);
+    struct mach_timebase_info timebase;
+    mach_timebase_info(&timebase);
 
-    return (mach_absolute_time() * convfact.numer) / (convfact.denom * 1000000);
+    auto machtime = mach_continuous_time();
+    auto numer = uint64_t (timebase.numer);
+    auto denom = uint64_t(timebase.denom);
+    auto monotonic_time = machtime * numer / denom / 100;
+    return monotonic_time / 10000;
 }
 #endif
 
