04324554cd44a9b943bcd145d62e11fb0ee4450c
Toofy <mostafa.alsari@gmail.com>
2023-01-15 16:39:36 +0000

    Correct placement of String in right method

--- launcher/core/src/sapphirelauncher.cpp
+++ launcher/core/src/sapphirelauncher.cpp
@@ -10,10 +10,7 @@ SapphireLauncher::SapphireLauncher(LauncherCore& window) : window(window), QObje
 void SapphireLauncher::login(const QString& lobbyUrl, const LoginInformation& info) {
     QJsonObject data{{"username", info.username}, {"pass", info.password}};
 
-    QUrl url;
-    url.setScheme("http");
-    url.setHost(lobbyUrl);
-    url.setPath("/sapphire-api/lobby/login");
+    QUrl url(lobbyUrl + "/sapphire-api/lobby/login");
 
     QNetworkRequest request(url);
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
@@ -39,7 +36,7 @@ void SapphireLauncher::login(const QString& lobbyUrl, const LoginInformation& in
 
 void SapphireLauncher::registerAccount(const QString& lobbyUrl, const LoginInformation& info) {
     QJsonObject data{{"username", info.username}, {"pass", info.password}};
-    QUrl url(lobbyUrl + "/sapphire-api/lobby/login");
+    QUrl url(lobbyUrl + "/sapphire-api/lobby/createAccount");
 
     QNetworkRequest request(url);
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
