c1646e20f6cbd711833cba1fb4fc6966d0035ee5
Joshua Goins <josh@redstrate.com>
2023-10-04 11:44:23 -0400

    Add script and CI to generate complete tarballs

---
+++ scripts/tarball.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# ensure the submodules are up to date
+git submodule init
+git submodule update
+
+# begin vendoring cargo dependencies
+cd external/libphysis
+cargo vendor ../../cargo-vendored
+mkdir .cargo
+cp ../../scripts/config.toml .cargo/config.toml
+cd ../../
+
+tar --exclude='*build*' --exclude='.idea' --exclude='.clang-format' --exclude='astra-source.tar.gz' --exclude-vcs -zcvf astra-source.tar.gz .
