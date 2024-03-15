177cb7637c1b8328b488ea4b704ca7ea74558058
Joshua Goins <josh@redstrate.com>
2024-03-15 19:06:24 -0400

    Change flatpak build script

--- scripts/build-flatpak.sh
+++ scripts/build-flatpak.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo &&
-flatpak install --user -y runtime/org.kde.Platform/x86_64/5.15-22.08 runtime/org.kde.Sdk/x86_64/5.15-22.08 org.freedesktop.Sdk.Extension.rust-stable/x86_64/22.08 &&
-flatpak-builder build --user --force-clean zone.xiv.astra.yml &&
+flatpak-builder build --user --force-clean --install-deps-from=flathub zone.xiv.astra.yml &&
 flatpak build-export export build &&
 flatpak build-bundle export astra.flatpak zone.xiv.astra --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
