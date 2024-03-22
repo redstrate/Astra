39a06d67fa5334ec410d526f569f0737a7de0f15
Joshua Goins <josh@redstrate.com>
2024-03-22 18:00:32 -0400

    Yet more build script improvements
    (cherry picked from commit 007db4cd2db7e9608def6deb189e2b3c253500c9)

--- scripts/tarball.sh
+++ scripts/tarball.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # ensure the submodules are up to date
-git submodule init
-git submodule update
+git submodule init &&
+git submodule update &&
 
 # begin vendoring cargo dependencies
-cd external/libphysis
-cargo vendor-filterer --platform=x86_64-unknown-linux-gnu ../../cargo-vendored
-mkdir .cargo
-cp ../../scripts/config.toml .cargo/config.toml
-cd ../../
+cd external/libphysis &&
+cargo vendor-filterer --platform=x86_64-unknown-linux-gnu ../../cargo-vendored &&
+mkdir .cargo &&
+cp ../../scripts/config.toml .cargo/config.toml &&
+cd ../../ &&
 
 tar --exclude='cmake-build*' --exclude='.idea' --exclude='.clang-format' --exclude='astra-source.tar.gz' --exclude-vcs -zcvf ../astra-source.tar.gz .
