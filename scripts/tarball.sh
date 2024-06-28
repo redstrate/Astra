5555960fd8afd86ba6af727364960d8d7be383fc
Joshua Goins <josh@redstrate.com>
2024-06-27 23:32:14 -0400

    Disable cargo vendoring for now
    This is breaking on the tempfile crate for some reason.

--- scripts/tarball.sh
+++ scripts/tarball.sh
@@ -5,10 +5,11 @@ git submodule init &&
 git submodule update &&
 
 # begin vendoring cargo dependencies
-cd external/libphysis &&
-cargo vendor-filterer --platform=x86_64-unknown-linux-gnu ../../cargo-vendored &&
-mkdir .cargo &&
-cp ../../scripts/config.toml .cargo/config.toml &&
-cd ../../ &&
+# TODO: should we really do this? an error blocked 0.6.0 release
+#cd external/libphysis &&
+#cargo vendor-filterer --platform=x86_64-unknown-linux-gnu ../../cargo-vendored &&
+#mkdir .cargo &&
+#cp ../../scripts/config.toml .cargo/config.toml &&
+#cd ../../ &&
 
 tar --exclude='cmake-build*' --exclude='.idea' --exclude='.clang-format' --exclude='astra-source.tar.gz' --exclude-vcs -zcvf ../astra-source.tar.gz .
