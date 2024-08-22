7ba1d4459e0803c484b20bf46aa4ad309451160a
Joshua Goins <josh@redstrate.com>
2024-08-22 18:29:38 -0400

    Add tests for more profile management
    Adding and removing profiles, and some of the profile index functions.

--- autotests/profilemanagertest.cpp
+++ autotests/profilemanagertest.cpp
@@ -21,6 +21,32 @@ private Q_SLOTS:
         QCOMPARE(profileManager.rowCount({}), 1);
         QCOMPARE(profileManager.numProfiles(), 1);
         QVERIFY(!profileManager.canDelete(profileManager.getProfile(0))); // the first profile can never be deleted
+
+        auto profile = profileManager.getProfile(0);
+
+        QCOMPARE(profileManager.getProfileByUUID(profile->uuid()), profile);
+        QCOMPARE(profileManager.getProfileIndex(profile->uuid()), 0);
+
+        QVERIFY(!profileManager.hasAnyExistingInstallations());
+    }
+
+    void testProfileManagement()
+    {
+        ProfileManager profileManager;
+
+        QCOMPARE(profileManager.rowCount({}), 0);
+        profileManager.load();
+
+        profileManager.addProfile();
+
+        QCOMPARE(profileManager.rowCount({}), 2);
+        QCOMPARE(profileManager.numProfiles(), 2);
+        QVERIFY(profileManager.canDelete(profileManager.getProfile(1)));
+
+        profileManager.deleteProfile(profileManager.getProfile(1));
+
+        QCOMPARE(profileManager.rowCount({}), 1);
+        QCOMPARE(profileManager.numProfiles(), 1);
     }
 };
 
