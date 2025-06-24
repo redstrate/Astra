7ea39bb5f975ae36b83feb395c23aed4b78f0c84
Joshua Goins <josh@redstrate.com>
2025-06-24 16:48:01 -0400

    Update tarball script

--- scripts/tarball.sh
+++ scripts/tarball.sh
@@ -4,12 +4,4 @@
 git submodule init &&
 git submodule update &&
 
-# begin vendoring cargo dependencies
-# TODO: should we really do this? an error blocked 0.6.0 release
-#cd external/libphysis &&
-#cargo vendor-filterer --platform=x86_64-unknown-linux-gnu ../../cargo-vendored &&
-#mkdir .cargo &&
-#cp ../../scripts/config.toml .cargo/config.toml &&
-#cd ../../ &&
-
-tar --exclude='cmake-build*' --exclude='.idea' --exclude='.clang-format' --exclude='astra-source.tar.gz' --exclude-vcs -zcvf ../astra-source.tar.gz .
+tar --exclude='cmake-build*' --exclude='.idea' --exclude='.clang-format' --exclude='astra-source.tar.gz' --exclude='.flatpak-builder' --exclude='export' --exclude='build' --exclude='astra.flatpak' --exclude-vcs -zcvf ../astra-source.tar.gz .
