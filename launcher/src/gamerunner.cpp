aa26ed82a97addf99adb97ad31fe370776b5c72b
Joshua Goins <josh@redstrate.com>
2025-12-07 13:38:18 -0500

    GameRunner: Escape spaces when building arguments
    Easily seen on Windows, if the user has a space in their name.
    
    Fixes #46

--- launcher/src/gamerunner.cpp
+++ launcher/src/gamerunner.cpp
@@ -286,7 +286,13 @@ QString GameRunner::getGameArgs(const Profile &profile, const std::optional<Logi
 
     QString argJoined;
     for (const auto &[key, value] : gameArgs) {
-        argJoined += argFormat.arg(key, value);
+        // We have to double-up on spaces, otherwise the encryption will destroy them. UserPath - for example - can contain spaces if the username does on
+        // Windows.
+        const auto escapeSpaces = [](QString string) {
+            return string.replace(QLatin1Char(' '), QStringLiteral("  "));
+        };
+
+        argJoined += argFormat.arg(escapeSpaces(key), escapeSpaces(value));
     }
 
     return !profile.config()->isBenchmark() && m_launcher.config()->encryptArguments() ? encryptGameArg(argJoined) : argJoined;
