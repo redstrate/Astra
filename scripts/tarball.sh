c65ddcec807664c659a1f3a3a9dec38685eac2bf
Joshua Goins <josh@redstrate.com>
2025-12-21 16:41:54 -0500

    Exclude more stuff from the tarball

--- scripts/tarball.sh
+++ scripts/tarball.sh
@@ -4,4 +4,4 @@
 git submodule init &&
 git submodule update &&
 
-tar --exclude='cmake-build*' --exclude='.idea' --exclude='.clang-format' --exclude='astra-source.tar.gz' --exclude-vcs -zcvf ../astra-source.tar.gz .
+tar --exclude='cmake-build*' --exclude='.idea' --exclude='.clang-format' --exclude='astra-source.tar.gz' --exclude='.flatpak-builder' --exclude='export' --exclude='build' --exclude='.cache' --exclude='compile_commands.json' --exclude='.kateproject.build' --exclude='.directory' --exclude='*.flatpak' --exclude-vcs -zcvf ../astra-source.tar.gz .
