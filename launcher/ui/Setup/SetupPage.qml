redstrate/Astra

commit 65b071ae5e79c2c77dca72dffda7a42846f2fd7e

Fix icon not showing up inside application on Windows
This mostly affects the Settings pages.

launcher/ui/Setup/SetupPage.qml
@@ -31,12 +31,14 @@ FormCard.FormCardPage {
         onAccepted: page.profile.config.gamePath = decodeURI(selectedFolder.toString().replace("file://", "").substr(Qt.platform.os === "windows" ? 1 : 0))
     }
 
-    Image {
-        source: "qrc:/zone.xiv.astra.svg"
+    Kirigami.Icon {
+        source: "zone.xiv.astra"
 
         fillMode: Image.PreserveAspectFit
         visible: !LauncherCore.profileManager.hasAnyExistingInstallations()
 
+        Layout.preferredWidth: Math.round(Kirigami.Units.iconSizes.huge * 1.5)
+        Layout.preferredHeight: Math.round(Kirigami.Units.iconSizes.huge * 1.5)
         Layout.fillWidth: true
         Layout.fillHeight: true
         Layout.margins: Kirigami.Units.largeSpacing * 3

CMakeLists.txt
@@ -59,17 +59,14 @@ if (BUILD_TESTING)
 endif()
 
 install(FILES zone.xiv.astra.appdata.xml DESTINATION ${KDE_INSTALL_METAINFODIR})
+install(FILES zone.xiv.astra.desktop DESTINATION ${KDE_INSTALL_APPDIR})
+install(FILES zone.xiv.astra.svg DESTINATION ${KDE_INSTALL_FULL_ICONDIR}/hicolor/scalable/apps)
 
-if (NOT WIN32)
-    install(FILES zone.xiv.astra.desktop DESTINATION ${KDE_INSTALL_APPDIR})
-    install(FILES zone.xiv.astra.svg DESTINATION ${KDE_INSTALL_FULL_ICONDIR}/hicolor/scalable/apps)
-
-    ecm_qt_install_logging_categories(
-            EXPORT ASTRA
-            FILE astra.categories
-            SORT DESTINATION ${KDE_INSTALL_LOGGINGCATEGORIESDIR}
-    )
-endif()
+ecm_qt_install_logging_categories(
+        EXPORT ASTRA
+        FILE astra.categories
+        SORT DESTINATION ${KDE_INSTALL_LOGGINGCATEGORIESDIR}
+)
 
 feature_summary(WHAT ALL INCLUDE_QUIET_PACKAGES FATAL_ON_MISSING_REQUIRED_PACKAGES)
 

launcher/CMakeLists.txt
@@ -147,16 +147,6 @@ qt_target_qml_sources(astra_static
         ui/Main.qml
 )
 
-set_source_files_properties(../zone.xiv.astra.svg PROPERTIES
-        QT_RESOURCE_ALIAS /zone.xiv.astra.svg
-)
-
-qt_target_qml_sources(astra_static
-        PREFIX /
-        RESOURCES
-        ../zone.xiv.astra.svg
-)
-
 target_link_libraries(astra PRIVATE
         astra_static
         astra_staticplugin

commit 95b4fbfe3970aacf0de6f57efe7a97f8d774ccd2

Fix SetupPage not loading properly
This was broken by 65b071ae5e79c2c77dca72dffda7a42846f2fd7e and I never
tested the page afterward, apparently.

launcher/ui/Setup/SetupPage.qml
@@ -34,7 +34,6 @@ FormCard.FormCardPage {
     Kirigami.Icon {
         source: "zone.xiv.astra"
 
-        fillMode: Image.PreserveAspectFit
         visible: !LauncherCore.profileManager.hasAnyExistingInstallations()
 
         Layout.preferredWidth: Math.round(Kirigami.Units.iconSizes.huge * 1.5)